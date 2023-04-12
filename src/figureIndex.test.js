66b72192d10ac3d44fba7e2271e8c7388c3c5562
YG1022 <958065532@qq.com>
2023-04-12 14:03:15 +0800

    test: should return array in 1-18278 range

--- src/figureIndex.test.js
+++ src/figureIndex.test.js
@@ -1,12 +1,16 @@
 const figureIndex = require('./figureIndex.js');
 
-test('should return array in 1-26 range', () => {
+test('should return array in 1-18278 range', () => {
     // Arrange
-    const figureResult = figureIndex(1, 2);
+    const figureResult1 = figureIndex(1, 2);
+    const figureResult2 = figureIndex(1, 1);
+    const figureResult3 = figureIndex(26, 3);
     // Act
 
     // Assert
-    expect(figureResult).toEqual(['A', 'B']);
+    expect(figureResult1).toEqual(['A', 'B']);
+    expect(figureResult2).toEqual(['A']);
+    expect(figureResult3).toEqual(['Z', 'AA', 'AB']);
 });
 
 test('should throw error if startIndex is less than 1', () => {
