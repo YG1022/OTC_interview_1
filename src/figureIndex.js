5c67f3dcfdd6722fc8062a11dcbbf989a2371fcd
YG1022 <958065532@qq.com>
2023-04-12 13:33:54 +0800

    feat: return proper array in 1-26 range

--- src/figureIndex.js
+++ src/figureIndex.js
@@ -1,3 +1,12 @@
-const figureIndex = 'figureIndex.js loaded';
+const figureIndex = (startIndex, count) => {
+  const originColumns = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const result = [];
+
+  for (let i = startIndex; i < startIndex + count; i += 1) {
+    result.push(originColumns[i - 1]);
+  }
+
+  return result;
+};
 
 module.exports = figureIndex;
