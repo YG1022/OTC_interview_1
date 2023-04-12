dc036646dfc799ce2257c636562d5aa4e92a977b
YG1022 <958065532@qq.com>
2023-04-12 13:23:42 +0800

    test: should return array in 1-26 range

--- src/figureIndex.test.js
+++ src/figureIndex.test.js
@@ -1,5 +1,10 @@
 const figureIndex = require('./figureIndex.js');
 
-test('should load figureIndex.js', () => {
-  expect(figureIndex).toBe('figureIndex.js loaded');
+test('should return array in 1-26 range', () => {
+  // Arrange
+  const figureResult = figureIndex(1, 2);
+  // Act
+
+  // Assert
+  expect(figureResult).toBe(['A', 'B']);
 });
